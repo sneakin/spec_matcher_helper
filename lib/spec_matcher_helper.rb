9541b14eaf748458afc1be133c2572f96d77eacd
Nolan Eakins <sneakin@semanticgap.com>
2008-06-29 23:01:58 -0400

    Dropped back down to the basic method. RSpec was taking over class variable resolution within the generated method.

--- lib/spec_matcher_helper.rb
+++ lib/spec_matcher_helper.rb
@@ -6,30 +6,6 @@
 
 require 'active_support'
 
-module SemanticGap
-  module Spec
-    class MatchMaker
-      def initialize(name, parent, &code)
-        generate_class(name, parent).class_eval(&code)
-        generate_method(name)
-      end
-      
-      def generate_class(name, parent)
-        klass = Class.new(parent ? parent : Object)
-
-        Object.const_set(name.to_s.camelize, klass)
-        @klass = klass
-      end
-      
-      def generate_method(name)
-        Object.send :define_method, name.to_s do |*args|
-          @klass.new(*args)
-        end
-      end
-    end
-  end
-end
-
 # Makes defining RSpec matchers a simpler affair. You still have to provide
 # the typical set of methods: initialize, matches?, etc. The one thing you
 # do not have to do is define a method to instantiate your matcher.
@@ -38,9 +14,18 @@ end
 # along with a method called {name}.
 #
 # If you need or want to inherit from another matcher, you can pass in a hash
-# with the first key being {name} and the super as the value.
+# with the first key being the name of the matcher and the value either being
+# another matcher's name or a class.
 def matcher(name, &code)
-  parent = nil
+  parent = Object
   name, parent = name.to_a.first if name.kind_of? Hash
-  SemanticGap::Spec::MatchMaker.new(name, parent, &code)
+  parent = Object.const_get(parent.to_s.camelize) if parent.kind_of? Symbol
+  
+  klass = Class.new(parent)
+  Object.const_set(name.to_s.camelize, klass)
+  klass.class_eval(&code)
+
+  Object.send :define_method, name.to_s do |*args|
+    klass.new(*args)
+  end
 end
--- spec/spec_matcher_spec.rb
+++ spec/spec_matcher_spec.rb
@@ -34,7 +34,7 @@ class InheritsFrom
   end
 end
 
-matcher :test_matcher_with_super => InheritsFrom do
+matcher :test_matcher_with_super_class => InheritsFrom do
   def initialize(expecting)  #:nodoc:all
     super(expecting)
   end
@@ -43,7 +43,16 @@ matcher :test_matcher_with_super => InheritsFrom do
   end
 end
 
-describe 'defmatcher', :shared => true do
+matcher :test_matcher_with_super_symbol => :inherits_from do
+  def initialize(expecting)  #:nodoc:all
+    super(expecting)
+  end
+  
+  def matches?(target) #:nodoc:all
+  end
+end
+
+describe 'matcher', :shared => true do
   describe 'generates a method using the given name that' do
     it "returns an instance of the generated class" do
       @matcher.should be_kind_of(@klass)
@@ -55,14 +64,14 @@ describe 'defmatcher', :shared => true do
   end
 end
 
-describe 'defmatcher', 'with test_matcher' do
+describe 'matcher', 'with test_matcher' do
   before(:each) do
     @args = [:a, :b]
     @klass = TestMatcher
     @matcher = test_matcher(*@args)
   end
 
-  it_should_behave_like 'defmatcher'
+  it_should_behave_like 'matcher'
 
   it "responds to all of the methods defined in the block" do
     %W(matches? expecting failure_message negative_failure_message).each do |meth|
@@ -71,14 +80,15 @@ describe 'defmatcher', 'with test_matcher' do
   end
 end
 
-describe 'defmatcher', 'with test_matcher_with_super' do
+describe 'matcher with super', :shared => true do
   before(:each) do
     @args = :arg
-    @klass = TestMatcherWithSuper
-    @matcher = test_matcher_with_super(*@args)
+    @klass = Object.const_get(@matcher_name.to_s.camelize)
+    @method = method(@matcher_name)
+    @matcher = @method.call(*@args)
   end
 
-  it_should_behave_like 'defmatcher'
+  it_should_behave_like 'matcher'
   
   it "responds to all of the methods defined in the block" do
     %W(matches? expecting).each do |meth|
@@ -89,4 +99,20 @@ describe 'defmatcher', 'with test_matcher_with_super' do
   it "subclasses InheritsFrom" do
     @matcher.should be_kind_of(InheritsFrom)
   end
+end
+
+describe 'matcher', 'with test_matcher_with_super_class' do
+  it_should_behave_like 'matcher with super'
+  
+  before(:all) do
+    @matcher_name = :test_matcher_with_super_class
+  end
+end
+
+describe 'matcher', 'with test_matcher_with_super_symbol' do
+  before(:all) do
+    @matcher_name = :test_matcher_with_super_symbol
+  end
+
+  it_should_behave_like 'matcher with super'
 end
